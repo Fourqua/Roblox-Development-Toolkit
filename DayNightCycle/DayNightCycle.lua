0c8337a56a94d5a9569da8dac20e1943b215594d
4qua <rraayy69420@gmail.com>
2026-02-23 18:20:12 -0700

    added DayNightCycle.lua

---
+++ DayNightCycle/DayNightCycle.lua
@@ -0,0 +1,34 @@
+-- services
+local Lighting = game:GetService("Lighting")
+local RunService = game:GetService("RunService")
+
+-- CONFIG: Change these to your liking.
+local CONFIG = {
+	DayLength = 240, -- length of a full day/night cycle in real seconds
+	StartTime = 8,   -- hour the server starts at (0-24 hours)
+}
+
+-- A full day is 24 hours. 1440 minutes
+local SECONDS_IN_DAY = 24 * 60
+
+-- Convert the starting hour into minutes. All tracking will be done in minutes
+local timeOfDay = CONFIG.StartTime * 60
+
+-- Run every frame on the server allowing all players to see the same time.
+RunService.Heartbeat:Connect(function(dt)
+
+	-- Advance time based on how much real time passed scaled to day length
+	timeOfDay = timeOfDay + (SECONDS_IN_DAY / CONFIG.DayLength) * dt
+
+	-- Loop back to midnight once we pass the end of the day
+	if timeOfDay >= SECONDS_IN_DAY then
+		timeOfDay = timeOfDay - SECONDS_IN_DAY
+	end
+
+	-- Split total minutes into hours and leftover minutes for clock
+	local hours = math.floor(timeOfDay / 60)
+	local minutes = math.floor(timeOfDay % 60)
+
+	-- Push the new time to lighting
+	Lighting:SetMinutesAfterMidnight(hours * 60 + minutes)
+end)
